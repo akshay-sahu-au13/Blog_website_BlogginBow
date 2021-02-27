c359fffd135cd5b525892adc1162ff6e31241d59
Akshay Sahu <akki5233@gmail.com>
2021-02-27 23:04:36 +0530

    Added bootstrap

--- index.js
+++ index.js
@@ -2,19 +2,31 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const path = require('path');
 const bodyParser = require('body-parser');
-
+const hbs = require('hbs');
+const userRoutes = require('./routes/user');
+const MongoInit = require('./config/mongodb');
 const layout = path.join('layouts', "index");
 
-const PORT = 4400;
+const PORT = process.env.PORT || 5100;
 
 const app = express();
 
-app.set('view engine', 'hbs');
-app.set(express.static('public'));
-app.set('views', path.join(__dirname,'views'));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use('/auth', userRoutes);
+app.use(express.static(path.join(__dirname,'public')));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'views'));
+// app.engine('hbs', hbs({
+//     defaultLayout: 'index',
+//     extname: '.hbs',
+//     layoutsDir: path.join(__dirname, 'layouts')
+//    }))
+
+app.get('/', (req, res)=> {
+    res.render('home', {title: " BlogginBow home", layout});
+});
 
 
 
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const path = require("path");
+const hbs = require('hbs');
+const User = require('../models/user');
+
+const layout = path.join('layouts','index');
+// const layout = path.join('layouts', "index");
+
+router.get('/signup', (req, res)=> {
+    res.render('signup', {title: "Signup", layout})
+});
 
 
-const layout = path.join('layouts', "index");
+router.get('/login', (req, res)=> {
+    res.render('login', {title: "Login", layout})
+})
 
+module.exports = router;
